9f452baca52f83da71c055f2e28dcd66c5eead48
permserg <permserg@gmail.com>
2023-01-06 21:54:18 +0300

    snack fab

--- app/src/main/java/com/example/bz1/MainActivity.kt
+++ app/src/main/java/com/example/bz1/MainActivity.kt
@@ -2,16 +2,29 @@ package com.example.bz1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         init()
+
+
     }
 
     private fun init(){
         setUpToolbar()
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
     }
 
     private fun setUpToolbar(){
