cf67a4fdc502ad22400ebdc6ccdff0036bd077a2
permserg <permserg@gmail.com>
2023-01-06 22:03:29 +0300

    snack fab1

--- app/src/main/java/com/example/bz1/MainActivity.kt
+++ app/src/main/java/com/example/bz1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bz1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +23,10 @@ class MainActivity : AppCompatActivity() {
         val fab: View = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
+                .setAction("Action", { view ->
+                    Toast.makeText(getApplicationContext(), "Next clicked!",Toast.LENGTH_LONG)
+                        .show()
+                })
                 .show()
         }
     }
